25865fddf069e35bfaa84cfaa743cabf7fa38256
enidmalesi <dyneduxe@gmail.com>
2022-09-09 23:51:34 +0300

    Learning low language

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - This is the main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	char ch;
+	float f;
+	long int li;
+	long long int lli;
+
+	printf("Size of an int: %li byte(s)\n", sizeof(i));
+	printf("Size of a char: %li byte(s)\n", sizeof(ch));
+	printf("Size of a float: %li byte(s)\n", sizeof(f));
+	printf("Size of a long int: %li byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %li byte(s)\n", sizeof(lli));
+
+	return (0);
+}
+
