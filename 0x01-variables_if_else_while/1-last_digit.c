811fa15b5238f764c48d478d7dc9d9be4fffca05
enidmalesi <dyneduxe@gmail.com>
2022-09-12 19:58:39 +0300

    This codes are for loops

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,30 +1,25 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
- * main - This is the main function
- * Return: 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
-	int l;
+	int n, last_d;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	l = n % 10;
-	if  (l > 5)
-	{
-		printf("%d is %d positive\n", n, l);
-	}
-	else if (l == 0)
-	{
-		printf("%d is %d zero\n", n, l);
-	}
-	else 
-	{
-		printf("%d is %d less negaative\n", n, l);
-	}
+	last_d = n % 10;
+	if (last_d > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_d);
+	else if (last_d == 0)
+		printf("Last digit of %d is %d and is 0\n", n, last_d);
+	else
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_d);
 	return (0);
 }
 
