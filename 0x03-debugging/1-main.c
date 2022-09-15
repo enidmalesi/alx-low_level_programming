752e37b7ea5c2122d82ae207bf520f044f0181ad
enidmalesi <dyneduxe@gmail.com>
2022-09-15 22:59:29 +0300

    this is a c script for debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,12 +9,13 @@ int main(void)
 int i;
 
 printf("Infinite loop incoming :(\n");
-/*
- *while (i < 10)
- *{
- *putchar(i);
- *}
-*/
+i=0;
+
+/*while (1 < 10) */
+{
+ /*putchar(i);*/
+}
+
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
