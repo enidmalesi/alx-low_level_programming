c66a6d17fd5c059dab1a3db52746ca0bfba6aa56
enidmalesi <dyneduxe@gmail.com>
2023-04-13 21:55:32 +0300

    Learning search algorithms

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -17,11 +17,11 @@ int linear_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 
-	for (i = 0; i < (size; i++)
+	for (i = 0; array && i < size; ++i)
 	{
-		printf("Value checked array[%u] = [%d]\n", i, array[i]);
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 
-		if (value == array[i])
+		if (array[i] == value)
 			return (i);
 	}
 	return (-1);
