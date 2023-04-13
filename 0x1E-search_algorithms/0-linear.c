ebddf5eafa1d7a63775c2be5c482c301a7a59832
enidmalesi <dyneduxe@gmail.com>
2023-04-13 22:41:46 +0300

    Learning search algorithms

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,28 +1,46 @@
+wilfried@0x1E-search_algorithms$ cat 0-main.c 
+#include <stdio.h>
+#include <stdlib.h>
 #include "search_algos.h"
 
 /**
- * linear_search - Searches for value in array of integers using Linear search
- * @array: Pointer to first element of array to search
- * @size: Number of elements in array
- * @value: Value to search
+ * main - Entry point
  *
- * Return: First index where value is found
- * -1 if value not in array or array is NULL
- *  Print value every time comparing value to that being searched for
+ * Return: Always EXIT_SUCCESS
  */
-int linear_search(int *array, size_t size, int value)
+int main(void)
 {
-	size_t i;
+    int array[] = {
+        10, 1, 42, 3, 4, 42, 6, 7, -1, 9
+    };
+    size_t size = sizeof(array) / sizeof(array[0]);
 
-	if (array == NULL)
-		return (-1);
-
-	for (i = 0; i < size; i++)
-	{
-		printf("Value checked array[%li] = [%i]\n", i, array[i]);
-
-		if (array[i] == value)
-			return (i);
-	}
-	return (-1);
+    printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
+    printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
+    printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
+    return (EXIT_SUCCESS);
 }
+wilfried@0x1E-search_algorithms$ gcc -Wall -Wextra -Werror -pedantic -std=gnu89 0-main.c 0-linear.c -o 0-linear
+wilfried@0x1E-search_algorithms$ ./0-linear 
+Value checked array[0] = [10]
+Value checked array[1] = [1]
+Value checked array[2] = [42]
+Value checked array[3] = [3]
+Found 3 at index: 3
+
+Value checked array[0] = [10]
+Value checked array[1] = [1]
+Value checked array[2] = [42]
+Found 42 at index: 2
+
+Value checked array[0] = [10]
+Value checked array[1] = [1]
+Value checked array[2] = [42]
+Value checked array[3] = [3]
+Value checked array[4] = [4]
+Value checked array[5] = [42]
+Value checked array[6] = [6]
+Value checked array[7] = [7]
+Value checked array[8] = [-1]
+Value checked array[9] = [9]
+Found 999 at index: -1
